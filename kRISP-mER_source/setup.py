e00bea3529755f34edc0f989d0c2cde30f75fae2
mahmudhera <mahmudhera93@gmail.com>
2020-07-30 19:27:35 +0600

    updated version number

--- kRISP-mER_source/setup.py
+++ kRISP-mER_source/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'krispmer',
-    version = '0.0.3',
+    version = '0.0.4',
     author= 'Mahmudur Rahman Hera',
     author_email= 'mahmudhera93@gmail.com',
     description = 'A tool to design CRISPR guideRNA without using reference genome',
